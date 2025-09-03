1268825e31fc6c5f57ee82dfde888940d734776c
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-03 16:41:10 +0000

    add dynamic vhost conf for alvinclient

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,6 @@
 : "${FITNESSE_CONTEXT_ROOT_ARG:=someContextRoot}"
 
 sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
+sed -i "s|{ALVIN_CLIENT_CONTEXT_ROOT}|${ALVIN_CLIENT_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
