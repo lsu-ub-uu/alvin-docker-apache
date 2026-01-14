45ef28bdf7550b3d758edf363bc056f34c3892ca
perebartrolisimo <pere.bartroli@ub.uu.se>
2026-01-14 12:42:43 +0000

    CORA-3084 Add server name on start of a pod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 : "${FITNESSE_CONTEXT_ROOT_ARG:=someContextRoot}"
 
+sed -i "s|{SERVER_NAME}|${SERVER_NAME_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 sed -i "s|{ALVIN_CLIENT_CONTEXT_ROOT}|${ALVIN_CLIENT_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 
